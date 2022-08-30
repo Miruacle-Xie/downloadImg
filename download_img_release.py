42198ca7444507bcce5cd44ca428b2b99e5da17f
Miruacle-Xie <53250282+Miruacle-Xie@users.noreply.github.com>
2022-08-30 12:21:44 +0800

    Add files via upload

--- download_img_release.py
+++ download_img_release.py
@@ -64,25 +64,38 @@ if __name__ == '__main__':
     print(ws.max_column)
     kv = {"user-agent": "Mozilla/5.0"}
     # path = "C:\\Users\\Administrator\\Desktop\\" + file + '\\'
-    path = file.replace("\"", "").split('.')[0] + '\\'
-    print(path)
-    mkdir(path)
+    keyIn = input("按回车:开始下载\n")
+    if '/' in keyIn:
+        path = keyIn[1:] + '\\'
+        print(path)
+        mkdir(path)
+    else:
+        path = file.replace("\"", "").split('.')[0] + '\\'
+        print(path)
+        mkdir(path)
+    # path = file.replace("\"", "").split('.')[0] + '\\'
+    # print(path)
+    # mkdir(path)
     # '''
-    print("正在开始下载...")
-    for i in range(2, ws.max_row + 1):
-        name = path + str(i-1) + '.jpg'
-        if ifDownloadFile(name):
-            url = ws.cell(i, 2).value
-            downResult = download_img(url, name)
-            if downResult != 0:
-                if downResult == ERROR_HTTPError:
-                    print("图片链接异常,请检查表格链接")
-                    break
-                elif downResult == ERROR_URLError and i == 2:
-                    print("请确保外网可用")
-                    break
-                elif downResult == ERROR_URLError and "链接无法找到" in ws.cell(i, 2).value:
-                    print("该链接无效")
-        else:
-            print(f"\r{i-1}.jpg Exist", end='')
-    # '''
+    try:
+        print("正在开始下载...")
+        for i in range(2, ws.max_row + 1):
+            name = path + str(i-1) + '.jpg'
+            if ifDownloadFile(name):
+                url = ws.cell(i, 2).value
+                downResult = download_img(url, name)
+                if downResult != 0:
+                    if downResult == ERROR_HTTPError:
+                        print("图片链接异常,请检查表格链接")
+                        break
+                    elif downResult == ERROR_URLError and i == 2:
+                        print("请确保外网可用")
+                        break
+                    elif downResult == ERROR_URLError and "链接无法找到" in ws.cell(i, 2).value:
+                        print("该链接无效")
+            else:
+                print(f"\r{i-1}.jpg Exist", end='')
+        input("\n已下载完成, 按回车键结束")
+        # '''
+    except Exception as e:
+        input(str(e)+"\n\n运行异常,按回车键结束")
